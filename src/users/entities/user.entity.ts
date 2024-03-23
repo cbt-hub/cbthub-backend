8cf84d8a6903651478b52f7a132306be82ba5913
jihyup-byun <jh_byun@opstech.kr>
2024-03-23 14:38:15 +0900

    feat: add user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,4 +1,13 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from 'typeorm';
+import { IsNotEmpty, IsEmail } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity()
 export class User {
@@ -6,8 +15,39 @@ export class User {
   id: number;
 
   @Column()
+  @IsEmail()
+  @IsNotEmpty()
+  @ApiProperty({ description: '사용자 이메일', example: 'user@example.com' })
+  email: string;
+
+  @Column({ unique: true })
+  @IsNotEmpty()
+  @ApiProperty({ description: '사용자 닉네임', example: 'nickname123' })
   nickname: string;
 
   @Column()
-  email: string;
+  @IsNotEmpty()
+  @ApiProperty({ description: '사용자 비밀번호', example: 'password' })
+  password: string;
+
+  @CreateDateColumn()
+  @ApiProperty({
+    description: '생성 날짜',
+    example: '2023-01-01T00:00:00.000Z',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  @ApiProperty({
+    description: '업데이트 날짜',
+    example: '2023-01-02T00:00:00.000Z',
+  })
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  @ApiProperty({
+    description: '삭제 날짜 (소프트 삭제)',
+    example: '2023-01-03T00:00:00.000Z',
+  })
+  deletedAt?: Date;
 }

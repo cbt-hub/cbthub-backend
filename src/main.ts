36ff3d0f02e526878c3d2aa862d0d74766d7236f
jihyup-byun <jh_byun@opstech.kr>
2024-03-23 13:56:12 +0900

    env: auth, users 도메인 추가, swagger 설정, auto reload 구현

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('CBT Hub API')
+    .setDescription('The CBT Hub API description')
+    .setVersion('1.0')
+    .addTag('auth', '관련된 인증 API들의 집합입니다.')
+    .addTag('users', '사용자 정보와 관련된 API들의 집합입니다!')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
+
 bootstrap();

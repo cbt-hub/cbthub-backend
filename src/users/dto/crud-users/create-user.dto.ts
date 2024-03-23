eb604d69aeb8e921738be5f0adca66591e8e611b
jihyup-byun <jh_byun@opstech.kr>
2024-03-23 15:39:30 +0900

    feat: add random string generator, update create-user dto

--- src/users/dto/crud-users/create-user.dto.ts
+++ src/users/dto/crud-users/create-user.dto.ts
@@ -1,17 +1,28 @@
 import { PickType } from '@nestjs/mapped-types';
 import { User } from '../../entities/user.entity';
 import { ApiProperty } from '@nestjs/swagger';
+import { generateRandomString } from 'libs/utils/gen.random.string';
+
 export class CreateUserDto extends PickType(User, [
   'email',
   'nickname',
   'password',
 ] as const) {
-  @ApiProperty({ description: 'The email of the user' })
+  @ApiProperty({
+    description: 'The email of the user',
+    example: `user${generateRandomString()}@example.com`,
+  })
   email: string;
 
-  @ApiProperty({ description: 'The nickname of the user' })
+  @ApiProperty({
+    description: 'The nickname of the user',
+    example: `nickname${generateRandomString()}`,
+  })
   nickname: string;
 
-  @ApiProperty({ description: 'The password of the user' })
+  @ApiProperty({
+    description: 'The password of the user',
+    example: `password${generateRandomString()}`,
+  })
   password: string;
 }

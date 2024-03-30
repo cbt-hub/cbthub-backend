c156c095bb3daeb6d9b3f26f7a88210604561389
jh_byun <jh_byun@opstech.kr>
2024-03-30 20:47:28 +0900

    feat: question solve 로직 추가

--- src/questions/service/questions.service.ts
+++ src/questions/service/questions.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Question } from '../entities/question.entity';
 import { Repository } from 'typeorm';
@@ -12,9 +12,14 @@ import { Round } from '../entities/round.entity';
 import { QuestionSolveDto } from '../dto/question/questionSolve.dto';
 import { decode } from 'jsonwebtoken';
 import { User } from '@src/users/entities/user.entity';
+import {
+  QuestionStatus,
+  QuestionStatusEnum,
+} from '../entities/questionStatus.entity';
 
 @Injectable()
 export class QuestionsService {
+  private readonly logger = new Logger(QuestionsService.name);
   constructor(
     @InjectRepository(Question)
     private questionRepository: Repository<Question>,
@@ -26,6 +31,8 @@ export class QuestionsService {
     private roundRepository: Repository<Round>,
     @InjectRepository(User)
     private userRepository: Repository<User>,
+    @InjectRepository(QuestionStatus)
+    private questionStatusRepository: Repository<QuestionStatus>,
   ) {}
 
   /**
@@ -101,18 +108,82 @@ export class QuestionsService {
     token: string,
   ) {
     checkNumberString(questionId);
-
-    const question = await this.questionRepository.findOne({
-      where: { id: Number(questionId) },
-      relations: ['round'],
-    });
-    console.log(`question: ${JSON.stringify(question)}`);
-
-    // token decoding 후 user uuid 추출 후 user 조회
     const userUuid = decode(token)['uuid'];
-    const user = await this.userRepository.findOneBy({ uuid: userUuid });
-    console.log(`user: ${JSON.stringify(user)}`);
 
-    // roundStatus 조회
+    // question 조회와 user 조회를 동시에 실행
+    const [question, user] = await Promise.all([
+      this.questionRepository.findOne({
+        where: { id: Number(questionId) },
+        relations: ['round'],
+      }),
+      this.userRepository.findOneBy({ uuid: userUuid }),
+    ]);
+
+    // questionId와 userId로 questionStatus 조회
+    const isNotNullQuestionStatus = await this.questionStatusRepository.findOne(
+      {
+        where: { question: { id: question.id }, user: { id: user.id } },
+      },
+    );
+
+    // 처음 푸는 문제인 경우, 해당 회차의 모든 문제에 대한 questionStatus 생성
+    if (!isNotNullQuestionStatus) {
+      const questions = await this.questionRepository.find({
+        where: { round: { id: question.round.id } },
+      });
+      this.logger.debug(`questions: ${JSON.stringify(questions)}`);
+      if (!questions || questions.length === 0) {
+        throw new Error('해당하는 문제가 없습니다.');
+      }
+
+      await Promise.all(
+        questions.map((question) => {
+          const questionStatus = new QuestionStatus();
+          questionStatus.question = question;
+          questionStatus.user = user;
+          return this.questionStatusRepository.save(questionStatus);
+        }),
+      );
+    }
+
+    /**
+     * @description questionDetails와 questionStatus를 조회하여 채점
+     * @link https://excalidraw.com/#json=-OCSQM1EgKGNC-POy12q_,Cbesr7h2NtFewDXSa6jUzA
+     * - 채점 로직(표)
+     */
+    let status = QuestionStatusEnum.SKIPPED;
+    const questionDetails = await this.questionDetailsRepository.findOne({
+      where: { id: Number(questionSolveDto.questionDetailsId) },
+    });
+    if (!questionDetails) {
+      throw new Error('해당하는 questionDetails가 없습니다.');
+    }
+    const questionStatus = await this.questionStatusRepository.findOne({
+      where: { question: { id: question.id }, user: { id: user.id } },
+    });
+    if (!questionStatus) {
+      throw new Error('해당하는 questionStatus가 없습니다.');
+    }
+
+    this.logger.debug(`questionDetails: ${JSON.stringify(questionDetails)}`);
+
+    if (!questionDetails.isCorrect) {
+      status = QuestionStatusEnum.SOLVED_WRONG;
+    } else if (
+      questionStatus.status === QuestionStatusEnum.SKIPPED ||
+      questionStatus.status === QuestionStatusEnum.SOLVED_CORRECT
+    ) {
+      status = QuestionStatusEnum.SOLVED_CORRECT;
+    } else {
+      status = QuestionStatusEnum.SOLVED_WRONG_CORRECT;
+    }
+
+    // questionStatus 업데이트
+    questionStatus.questionDetailsId = Number(
+      questionSolveDto.questionDetailsId,
+    );
+    questionStatus.status = status;
+    questionStatus.updatedAt = new Date();
+    return this.questionStatusRepository.save(questionStatus);
   }
 }
